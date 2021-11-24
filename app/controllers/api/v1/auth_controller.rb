3d659d2a4f648ef0a9570847fde61e8b92dd6d61
AleYanc <aleyanc2000@gmail.com>
2021-11-24 15:38:07 -0300

    OT112-24: Created Auth controller with create and destroy actions for login and logout functionalities

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,27 +7,29 @@ class Api::V1::AuthController < ApplicationController
   def create
     if user
       raise AuthenticationError unless user.authenticate(params.require(:password))
+
       token = AuthTokenService.call(user.id)
       render json: user.as_json(only: [:email]).merge("token": token)
     else
-      render json: {ok: false}
+      render json: { ok: false }
     end
   end
 
   def destroy
-    head(:ok, status: :no_content) 
+    head(:ok, status: :no_content)
   end
 
   private
-    def user
-      @user ||= User.find_by(email: params.require(:email))
-    end
+  
+  def user
+    @user ||= User.find_by(email: params.require(:email))
+  end
 
-    def parameter_missing(e)
-      render json: {error: e.message}, status: :unprocessable_entity
-    end
+  def parameter_missing(e)
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 
-    def handle_unauthenticated
-      render json: {ok: false}
-    end
+  def handle_unauthenticated
+    render json: { ok: false }
+  end
 end
