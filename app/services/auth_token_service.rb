70f8a15167e2d70cf3066e9f97fb41eae98dac6b
AleYanc <aleyanc2000@gmail.com>
2021-11-24 15:38:43 -0300

    OT112-24: Added auth token service with call and decode methods

---
+++ app/services/auth_token_service.rb
@@ -0,0 +1,15 @@
+class AuthTokenService
+
+  def self.call(user_id)
+    id = {user_id: user_id}
+    JWT.encode({
+      id: id,
+      exp: 3.hours.from_now.to_i}, ENV.fetch("HMAC_SECRET"), ENV.fetch("ALGORITHM_TYPE"))
+  end
+
+  def self.decode(token)
+    decoded_token = JWT.decode token, ENV.fetch("HMAC_SECRET"), true, {algorithm: ENV.fetch("ALGORITHM_TYPE")}
+    decoded_token[0]['user_id']
+  end
+
+end 
