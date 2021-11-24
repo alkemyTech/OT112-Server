23c0737214ee9f724b0abcdb30633028125e5c5f
AleYanc <aleyanc2000@gmail.com>
2021-11-24 15:55:25 -0300

    OT112-23: Added register route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      post 'auth/register', to: 'users#create'
+      post 'auth/login', to: 'auth#create'
+      delete 'auth/logout', to: 'auth#destroy', as: 'logout'
+    end
+  end
 end
