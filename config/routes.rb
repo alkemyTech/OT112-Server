8ec0f4d39fcdce34e1ac0f7b155930a172b4905e
jorge <jorge.garcia.mdz@gmail.com>
2021-11-25 19:01:36 -0300

    OT112-22: agregando path a routes de organization/public

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       post 'auth/register', to: 'users#create'
       post 'auth/login', to: 'auth#create'
       # delete 'auth/logout', to: 'auth#destroy'
+      get 'organization/public'
     end
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
