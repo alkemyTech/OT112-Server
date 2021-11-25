c9fbb4a103bbc8cdeb0e9a007d696e025e0caeaa
AleYanc <aleyanc2000@gmail.com>
2021-11-24 21:20:31 -0300

    OT112-24: Add user resources routes only to show and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+
       post 'auth/register', to: 'users#create'
       # post 'auth/login', to: 'auth#create'
       # delete 'auth/logout', to: 'auth#destroy'
+=======
+      resources :users
+      post 'auth/login', to: 'auth#create'
+      delete 'auth/logout', to: 'auth#destroy', as: 'logout'
+>>>>>>> OT112-24: Add user resources routes only to show and destroy action
     end
   end
 =======
