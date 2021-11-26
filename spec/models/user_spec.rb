57ec291722a71d0749b438d4cd84d151193340da
Fernando Ciballero <fernandociballero@gmail.com>
2021-11-26 00:14:23 -0300

    Add user unit specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  deleted_at      :datetime
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  photo           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  role_id         :integer
+#
+# Indexes
+#
+#  index_users_on_deleted_at  (deleted_at)
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
