4a547d3896107212944be509fa6c8cf756dd7d17
jorge <jorge.garcia.mdz@gmail.com>
2021-11-26 18:58:46 -0300

    OT112-22: agregando path a routes de organization/public mediante namespace

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
       get 'organization/public'
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
