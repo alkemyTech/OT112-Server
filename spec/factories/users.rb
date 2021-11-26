66bde7ecf6d0379aa0f99e1a0540c6fa83bb205e
Fernando Ciballero <fernandociballero@gmail.com>
2021-11-26 00:13:21 -0300

    Add user factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  deleted_at      :datetime
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  photo           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  role_id         :integer
+#
+# Indexes
+#
+#  index_users_on_deleted_at  (deleted_at)
+#
+FactoryBot.define do
+  factory :user do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+    photo { "MyString" }
+  end
+end
