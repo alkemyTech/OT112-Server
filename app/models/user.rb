7d974e8e01f32dd1876aa56a03e6b528233855f7
Fernando Ciballero <fernandociballero@gmail.com>
2021-11-26 00:06:38 -0300

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  deleted_at      :datetime
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  photo           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  role_id         :integer
+#
+# Indexes
+#
+#  index_users_on_deleted_at  (deleted_at)
+#
+
+class User < ApplicationRecord
+  acts_as_paranoid
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: {case_Sensitive: false}
+end
