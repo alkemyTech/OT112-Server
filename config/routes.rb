8efecbc75915749b13d7853dcaa45450985cb71c
AleYanc <aleyanc2000@gmail.com>
2021-11-26 21:17:10 -0300

    OT112-24: Delete inexistent order_params method from users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-
       post 'auth/register', to: 'users#create'
       # post 'auth/login', to: 'auth#create'
       # delete 'auth/logout', to: 'auth#destroy'
-
-      resources :users
-      post 'auth/login', to: 'auth#create'
-      delete 'auth/logout', to: 'auth#destroy', as: 'logout'
     end
   end
-=======
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
 end
