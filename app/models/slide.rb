a4ae1e7191f6474f6d441d67df412fadee220650
Franco <francoezapata1@gmail.com>
2021-11-28 19:01:45 -0300

    OT112-28:Add slide model and migration

---
+++ app/models/slide.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: slides
+#
+#  id              :bigint           not null, primary key
+#  order           :integer
+#  text            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :bigint           not null
+#
+# Indexes
+#
+#  index_slides_on_organization_id  (organization_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
+class Slide < ApplicationRecord
+  has_one_attached :image
+  belongs_to :organization
+end
