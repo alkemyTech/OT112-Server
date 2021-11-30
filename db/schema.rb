3c9fdae64891002b685ba286ffe0721de0731dfc
Fernando Ciballero <fernandociballero@gmail.com>
2021-11-30 20:33:06 -0300

    fix soft deletion in category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_033423) do
+ActiveRecord::Schema.define(version: 2021_11_30_213158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2021_11_27_033423) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_categories_on_deleted_at"
   end
 
   create_table "members", force: :cascade do |t|
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,10 +3,15 @@
 # Table name: categories
 #
 #  id          :bigint           not null, primary key
+#  deleted_at  :datetime
 #  description :string
 #  name        :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_deleted_at  (deleted_at)
 #
 require 'rails_helper'
 
