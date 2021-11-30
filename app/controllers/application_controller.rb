7586d119ee16c432b85a871c016f3f776fd398e1
AleYanc <aleyanc2000@gmail.com>
2021-11-30 13:04:45 -0300

    (fix)OT112-33: Fixed rescue_from CanCan as it was trying to render a flash message and redirecting
    OT112-33: Add middleware to application.rb
    
    OT112-33: Create ownership middleware
    
    OT112-33: added ownership method to authenticate_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,22 @@ class ApplicationController < ActionController::API
     begin
       @decoded = AuthTokenService.decode(header)
       @current_user = User.find(@decoded['user_id'])
+      ownership(@current_user)
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
     end
   end
+
+  def ownership(current_user)
+    token_id = current_user.id
+    if token_id != params[:id].to_i && current_user.role.name != 'admin'
+      render json: {msg: 'You are not allowed to see this content'}, status: :forbidden
+    end
+  end
     
   rescue_from CanCan::AccessDenied do
-    flash[:error] = 'Access denied!'
-    redirect_to root_url
+    render json: {msg: 'Acces denied!'}
   end
 end
