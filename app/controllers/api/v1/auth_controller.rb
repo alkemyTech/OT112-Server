29f09d97aab4bee531eea72de93299272c6cc95c
jorge <jorge.garcia.mdz@gmail.com>
2021-11-30 21:03:06 -0300

    feat-OT112-36: agregando show para devolver datos del usuario logueado

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,12 +4,22 @@ class Api::V1::AuthController < ApplicationController
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
   rescue_from AuthenticationError, with: :handle_unauthenticated
 
+  before_action :authorize_request, only: [:show]
+
   def create
     if user
       raise AuthenticationError unless user.authenticate(params.require(:password))
 
       token = AuthTokenService.call(user.id)
       render json: user.as_json(only: [:email]).merge(token: token)
+    else
+      render json: { ok: false }
+    end
+  end
+
+  def show
+    if @current_user
+      render json: UserSerializer.new(@current_user).serializable_hash.to_json
     else
       render json: { ok: false }
     end
