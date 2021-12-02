dfcd252211c8e591d94d3f65351f57a865e81ca4
Franco <francoezapata1@gmail.com>
2021-12-01 21:54:21 -0300

    OT112-42: Add endpoint create categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :authorize_request
+  
+  def create
+    @category = Category.new(category_params)
+    if params[:name] && params[:name].is_a?(String)
+      if @category.save
+          render json: CategorySerializer.new(@category).serializable_hash.to_json
+      else
+          render json: @category.errors, status: :unprocessable_entity
+      end
+        
+    else
+      render json: @category.errors, status: :unprocessable_entity 
+    end
+  end
+
+  private
+    def category_params
+      params.permit(:name, :description)
+    end
+
 end
