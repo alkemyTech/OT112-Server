469005cbf820b7ded287e9b4080cd4b06b9464c1
gervic08 <germanvicentin@gmail.com>
2021-12-02 16:13:36 -0300

    add-update-method-to-announcements-controller

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::AnnouncementsController < ApplicationController
+  before_action :set_announcement, only: [:update]
+
+  def update
+    if @announcement.update(announcement_params)
+      render json: AnnouncementSerializer.new(@announcement).serializable_hash.to_json
+    else
+      render json: @announcement.errors, status: :unprocessable_entity
+    end
+  end
+  
+
+  private
+
+    def set_announcement
+      @announcement = Announcement.find(params[:id])
+    end
+
+    def announcement_params
+      params.permit(:image, :name, :content, :category_id)
+    end
 end
