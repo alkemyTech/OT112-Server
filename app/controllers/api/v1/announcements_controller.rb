fb51fe469e0ea0a8161e8ec066c5295d838b1374
Victor Jara <Chogo01@hotmail.com>
2021-12-02 20:40:14 -0300

    OT112-49: Destoy method added

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -5,4 +5,13 @@ class Api::V1::AnnouncementsController < ApplicationController
     @announcement = Announcement.find(params[:id])
     render json: AnnouncementSerializer.new(@announcement).serializable_hash.to_json
   end
+
+  def destroy
+    @announcement = Announcement.find(params[:id])
+    if @announcement.destroy
+      head :no_content
+    else
+      render json: @announcement.errors, status: 422
+    end
+  end
 end
