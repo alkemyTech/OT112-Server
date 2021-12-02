47f057b36637631b1ff833fe0bd51ed910c9d8ca
Victor Jara <Chogo01@hotmail.com>
2021-12-02 11:36:58 -0300

    OT112-45: Announcement controller added

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::AnnouncementsController < ApplicationController
+  before_action :authorize_request
+
+  def show
+    @announcement = Announcement.find(params[:id])
+    render json: UserSerializer.new(@announcement).serializable_hash.to_json
+  end
 end
