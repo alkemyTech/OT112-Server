ad60092e05e86a7d8b7f4b3fd39631fae4807601
Franco <francoezapata1@gmail.com>
2021-12-03 21:17:37 -0300

    OT112-42: Made a modification into name validation

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,23 +1,27 @@
 class Api::V1::CategoriesController < ApplicationController
-  before_action :authorize_request
+  #before_action :authorize_request
   
   def create
     @category = Category.new(category_params)
-    params[:name].to_i.class != Integer
+    if !is_integer?(params[:name])
       if @category.save
         render json: CategorySerializer.new(@category).serializable_hash.to_json
       else
         render json: @category.errors, status: :unprocessable_entity
       end
         
-    # else
-    #   render json: @category.errors, status: :unprocessable_entity 
-    # end
+    else
+      render json: @category.errors, status: :unprocessable_entity 
+    end
   end
 
   private
     def category_params
       params.permit(:name, :description)
+    end
+
+    def is_integer?(p)
+      p.to_i.to_s == p
     end
 
 end
