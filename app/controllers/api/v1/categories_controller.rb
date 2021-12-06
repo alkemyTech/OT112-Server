0d24a6cef57c3288f94b72f83a6c591793547e16
Franco <68574311+zFranco1@users.noreply.github.com>
2021-12-05 22:22:50 -0300

    OT112-42: Create categories endpoint (#31)
    * Feat ot112 48 crear migracion y modelo de actividades (#30)
    
    * OT112-48: Activity model base specs created.
    
    * OT112-48: Activity base model created.
    
    * OT112-48: Activities base factory created.
    
    * OT112-48: Activities first migration created.
    
    * OT112-42: Add create endpoint for categories controller
    
    * OT112-42: Add categories serializer
    
    * OT112-42: Add routes for endpoint
    
    * OT112-42: Made a modification into name validation
    
    * OT112-42: Made a modification into name validation
    
    * OT112-42: Add endpoint create categories
    
    * OT112-42: Add category serializer
    
    Co-authored-by: Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
    Co-authored-by: Fernando Ciballero <fernandociballero@gmail.com>

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :authorize_request
+
+  def create
+    @category = Category.new(category_params)
+    if admin?(@current_user) && !is_integer?(params[:name])
+      if @category.save
+        render json: CategorySerializer.new(@category).serializable_hash.to_json
+      else
+        render json: @category.errors, status: :unprocessable_entity
+      end
+    else
+      render json: @category.errors, status: :unprocessable_entity 
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:name, :description)
+  end
+
+  def is_integer?(p)
+    p.to_i.to_s == p
+  end
 end
