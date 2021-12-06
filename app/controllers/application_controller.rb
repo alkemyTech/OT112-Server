3295a26206a55e3ffe6ab24ef0c023bbe88473fc
jorge <jorge.garcia.mdz@gmail.com>
2021-12-06 08:24:00 -0300

    OT112-35: eliminando linea ownership, persiste despues de realizar un rebase de main dentro de la rama 35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::API
     begin
       @decoded = AuthTokenService.decode(header)
       @current_user = User.find(@decoded['user_id'])
-      ownership(@current_user)
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
