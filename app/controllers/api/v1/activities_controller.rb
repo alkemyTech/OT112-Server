ba66496981cc7bcb577d163bfcde615c2c607b1f
Fernando Ciballero <fernandociballero@gmail.com>
2021-12-06 00:51:23 -0300

    fix controller serialization

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::ActivitiesController < ApplicationController
-  # POST /api/v1/activities
+  before_action :authorize_request
+
   def create
     @activity = Activity.new(activity_params)
 
     if @activity.save
-      render json: @activity, status: :created
+      render json: ActivitySerializer.new(@activity).serializable_hash.to_json, status: :created
     else
       render json: @activity.errors, status: :unprocessable_entity
     end
   end
 
   private
-    def activity_params
-      params.permit(:name, :content)
-    end
+
+  def activity_params
+    params.permit(:name, :content)
+  end
 end
