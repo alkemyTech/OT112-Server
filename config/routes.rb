c9dc556b5472f7c913b1f8945a51694ac93f7c06
AleYanc <aleyanc2000@gmail.com>
2021-12-07 14:26:52 -0300

    OT112-52: Added update endpoint to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :activities, only: %i[create update]
       resources :announcements, only: %i[show create update destroy]
       post 'auth/register', to: 'users#create'
       post 'auth/login', to: 'auth#create'
@@ -10,7 +11,6 @@ Rails.application.routes.draw do
       get 'organizations/:id/public', to: 'organizations#public'
       post 'organizations/public', to: 'organizations#create'
       resources :users, only: %i[update destroy]
-      post 'activities', to: 'activities#create'
     end
   end
 end
