4519d726ef4a576f2d2397e3b2a8fd1467fc0098
jorge <jorge.garcia.mdz@gmail.com>
2021-12-07 18:57:59 -0300

    feat-OT112-53: add seeders Acitivity to db/seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seed acivity
+activities = [
+  # ["name", "conent"]
+  ["activity number 1", "content activity number 1"],
+  ["activity number 2", "content activity number 2"],
+  ["activity number 3", "content activity number 3"],
+  ["activity number 4", "content activity number 4"],
+  ["activity number 5", "content activity number 5"],
+  ["activity number 6", "content activity number 6"],
+  ["activity number 7", "content activity number 7"]
+]
+
+activities.each do |name, content|
+  Activity.create(name: name, content: content)
+end
