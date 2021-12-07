a7b1b24314bf016c9f6ed481c77470ebddd0f112
AleYanc <aleyanc2000@gmail.com>
2021-12-07 14:26:32 -0300

    OT112-52: Added update method and error handling when provided id does not exist

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ActivitiesController < ApplicationController
   before_action :authorize_request
+  before_action :set_activity, only: [:update]
 
   def create
     @activity = Activity.new(activity_params)
@@ -11,7 +12,21 @@ class Api::V1::ActivitiesController < ApplicationController
     end
   end
 
+  def update
+    if admin?(@current_user) && @activity.update(activity_params)
+      render json: ActivitySerializer.new(@activity).serializable_hash.to_json, status: :ok
+    else
+      render json: admin?(@current_user) ? @activity.errors : {error: 'You are not authorized to perform that action'}, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "Could not find activity with ID '#{params[:id]}'"}
+  end
 
   def activity_params
     params.permit(:name, :content)
