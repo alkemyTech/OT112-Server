2ce7d604a1a59283566cb100fde627c439923631
jorge <jorge.garcia.mdz@gmail.com>
2021-12-07 22:02:59 -0300

    feat-OT112-54: add spect model contact

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :bigint           not null, primary key
+#  deleted_at :datetime
+#  email      :string           not null
+#  message    :text             not null
+#  name       :string           not null
+#  phone      :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
