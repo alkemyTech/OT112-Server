39d04a1de95bc61e21d47f09f452a6cd7df6e001
Franco <francoezapata1@gmail.com>
2021-12-08 13:46:52 -0300

    OT112-55: Add contacts controller and endpoint

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ContactsController < ApplicationController
+  before_action :authorize_request
+
+  def create
+    @contact = Contact.new(contact_params)
+    if params[:name] && params[:email]
+      if @contact.save
+        render json: ContactSerializer.new(@contact).serializable_hash.to_json
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end 
+
+  private
+    def contact_params
+      params.permit(:name, :email, :phone, :message)
+    end
+
+end
