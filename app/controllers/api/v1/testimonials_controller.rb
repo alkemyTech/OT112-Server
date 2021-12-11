5d3679ea09b6814952d362d1edf67f7e5a54d21f
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-12-11 19:54:07 -0300

    OT112-68: Added Update action to Testimonials controller. Fix response code an bad params.

--- app/controllers/api/v1/testimonials_controller.rb
+++ app/controllers/api/v1/testimonials_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::TestimonialsController < ApplicationController
+  before_action :set_testimonial, only: [:update]
   before_action :authorize_request
-  
+
   def create
     @testimonial = Testimonial.new(testimonial_params)
     if admin?(@current_user) && @testimonial.save
+      render json: TestimonialSerializer.new(@testimonial).serializable_hash.to_json, status: :created
+    else
+      render json: @testimonial.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if admin?(@current_user) && @testimonial.update(testimonial_params)
       render json: TestimonialSerializer.new(@testimonial).serializable_hash.to_json, status: :created
     else
       render json: @testimonial.errors, status: :unprocessable_entity
@@ -25,10 +34,13 @@ class Api::V1::TestimonialsController < ApplicationController
           },
           status: :ok
       end
-    end
-  end
+
 
   private
+
+  def set_testimonial
+    @testimonial = Testimonial.find(params[:id])
+  end
 
   def testimonial_params
     params.permit(:name, :content, :image)
