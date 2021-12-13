dd10e9a0fba65843caa8f8e978959c6ad48c95e6
Fernando Ciballero <fernandociballero@gmail.com>
2021-12-13 01:21:13 -0300

    fix: edit routes after rebase

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :activities, only: %i[create update]
       resources :announcements, only: %i[show create update destroy]
-      resources :announcements, only: %i[show create update destroy]
-      get 'announcements', to: 'announcements#show'
       post 'auth/register', to: 'users#create'
       post 'auth/login', to: 'auth#create'
       get 'auth/me', to: 'auth#show'
@@ -12,13 +10,9 @@ Rails.application.routes.draw do
       resources :categories, only: %i[index show create update destroy]
       get 'organizations/:id/public', to: 'organizations#public'
       post 'organizations/public', to: 'organizations#create'
-      resources 'slides', to: 'slides#index'
+      resources :slides, only: %i[index create]
       resources :testimonials, only: %i[create destroy]
       resources :users, only: %i[index update destroy]
-      resources :users, only: :update
-      post 'activities', to: 'activities#create'
-      post 'slides', to: 'slides#create'
-      
     end
   end
 end
