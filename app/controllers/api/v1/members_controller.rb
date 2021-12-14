f1063c641b9b87673ecf1d58a32b0732c5017afd
jorge <jorge.garcia.mdz@gmail.com>
2021-12-14 20:09:31 -0300

    feat-OT112-72: add method create to member controller

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::MembersController < ApplicationController
+  before_action :authorize_request
+
+  def create
+    @member = Member.new(member_params)
+    if is_string?(params[:name]) && @member.save
+
+    else
+      render json: @member.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def member_params
+    params.permit(:name)
+  end
+
+  def is_string?(param)
+    param.class == String
+  end
 end
