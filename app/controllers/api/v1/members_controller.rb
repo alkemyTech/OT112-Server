e3b984d31b95715a28f7489d31158443eb019dd6
jorge <jorge.garcia.mdz@gmail.com>
2021-12-15 20:53:49 -0300

    feat-OT112-72: validate admin user to create members

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -3,16 +3,21 @@ class Api::V1::MembersController < ApplicationController
 
   def create
     @member = Member.new(member_params)
-    if is_string?(params[:name]) && @member.save
+    
+    if admin?(@current_user)
+      if is_string?(params[:name]) && @member.save
 
+      else
+        render json: @member.errors, status: :unprocessable_entity
+      end
     else
-      render json: @member.errors, status: :unprocessable_entity
+      render json: {error: 'You are not authorized to perform that action'}, status: :unauthorized
     end
   end
 
   private
   def member_params
-    params.permit(:name)
+    params.permit(:name, :facebook_url, :instagram_url, :linkedin_url)
   end
 
   def is_string?(param)
