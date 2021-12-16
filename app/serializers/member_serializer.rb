a841aa4ba6c6d79276c69d1e2266c4df7d7e3e4a
JorgeGarciaMdz <37845619+JorgeGarciaMdz@users.noreply.github.com>
2021-12-15 23:12:52 -0300

    Feat ot112 72 endpoint creacion miembros (#68)
    * fix:update Gemfile lock
    
    * feat-OT112-72: add method create to member controller
    
    * feat-OT112-72: add route for members
    
    * feat-OT112-72: validate admin user to create members
    
    * feat-OT112-72: add response serializer to create member
    
    * feat-OT112-72: create member serializer
    
    * feat-OT112-72: add method create to member controller
    
    * feat-OT112-72: omitiendo parametro de redes sociales en member controller
    
    * fix controller serializer and routes
    
    Co-authored-by: Fernando Ciballero <fernandociballero@gmail.com>

---
+++ app/serializers/member_serializer.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id            :bigint           not null, primary key
+#  deleted_at    :datetime
+#  description   :text
+#  facebook_url  :string
+#  instagram_url :string
+#  linkedin_url  :string
+#  name          :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_members_on_deleted_at  (deleted_at)
+#
+class MemberSerializer
+  include JSONAPI::Serializer
+  attributes :name, :facebook_url, :instagram_url, :linkedin_url
+end
