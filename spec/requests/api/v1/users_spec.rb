956de6d4488ec5f4b0e9a7e3239673695138b92a
AleYanc <aleyanc2000@gmail.com>
2021-12-23 21:33:14 -0300

    OT112-95: Optimized 1
    OT112-95: Code optimization
    
    OT112-95: Optimizations #3

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users', type: :request do
-  let(:role) { FactoryBot.create(:role, name: 'admin', description: 'admin user') }
+  let(:role) { FactoryBot.create(:role) }
   let(:role2) { FactoryBot.create(:role, name: 'user', description: 'user user') }
 
-  let(:user) do
-    FactoryBot.create(:user, first_name: 'abc', last_name: 'abc', email: 'abc@abc.com', password: '123456', role: role)
-  end
-  let(:user2) do
-    FactoryBot.create(:user, first_name: 'abc5', last_name: 'abc5', email: 'abc5@abc.com', password: '123456',
-                             role: role2)
-  end
-
-  let(:token) { AuthTokenService.call(user.id) }
-  let(:token2) { AuthTokenService.call(user2.id) }
+  let(:user) { FactoryBot.create(:user, role: role) }
+  let(:user2) { FactoryBot.create(:user, email: 'test22@test.com', role: role2) }
+  
+  let(:good_auth_header) {
+    { "Authorization" => AuthTokenService.call(user.id).to_s }
+  }
+  let(:bad_auth_header) {
+    { "Authorization" => AuthTokenService.call(user2.id).to_s }
+  }
 
   it 'returns three users (GET /index)' do
     FactoryBot.create(:user, first_name: 'abc2', last_name: 'abc2', email: 'abc2@abc.com', password: '123456',
@@ -21,34 +20,34 @@ RSpec.describe 'Api::V1::Users', type: :request do
     FactoryBot.create(:user, first_name: 'abc3', last_name: 'abc3', email: 'abc3@abc.com', password: '123456',
                              role: role)
 
-    get '/api/v1/users', params: {}, headers: { 'Authorization' => token.to_s }
+    get api_v1_users_url, params: {}, headers: good_auth_header 
     expect(response).to have_http_status(:success)
     parsed = JSON.parse(response.body)
     expect(parsed['data'].size).to eq(3)
   end
 
   it 'returns unauthorized error when the user is not an admin' do
-    get '/api/v1/users', params: {}, headers: { 'Authorization' => token2.to_s }
+    get api_v1_users_url, params: {}, headers: bad_auth_header 
     expect(response).to have_http_status(:unauthorized)
     parsed = JSON.parse(response.body)
     expect(parsed['status']).to eq('Only admin users can list announcements')
   end
 
   it 'returns http status 401 when no authorization header is sent (GET /index)' do
-    get '/api/v1/users', params: {}, headers: {}
+    get api_v1_users_url, params: {}, headers: {}
     expect(response).to have_http_status(:unauthorized)
   end
 
   it 'creates a new user (POST /auth/register)' do
     expect do
-      post '/api/v1/auth/register',
+      post api_v1_auth_register_url,
            params: { first_name: 'abc4', last_name: 'abc4', email: 'abc4@abc.com', password: '123456', role: role }
     end.to change { User.count }.from(0).to(1)
     expect(response).to have_http_status(:success)
   end
 
   it 'returns an error message if there are missing param (POST /auth/register)' do
-    post '/api/v1/auth/register',
+    post api_v1_auth_register_url,
          params: { first_name: '', last_name: '', email: 'abc4@abc.com', password: '123456', role: role }
     expect(response).to have_http_status(:unprocessable_entity)
     parsed = JSON.parse(response.body)
@@ -57,9 +56,9 @@ RSpec.describe 'Api::V1::Users', type: :request do
   end
 
   it 'updates an existing user (PUT /api/v1/users/:id)' do
-    put "/api/v1/users/#{user.id}",
-        params: { first_name: 'abca', last_name: 'abca', email: 'abc4@abc.com', password: '123456', role: role }, 
-        headers: { 'Authorization' => token.to_s }
+    put api_v1_user_url(user.id),
+        params: {first_name: 'abca', last_name: 'abca', email: 'abc4@abc.com', password: '123456', role: role }, 
+        headers: good_auth_header
     expect(response).to have_http_status(:success)
     parsed = JSON.parse(response.body)
     # check if the updated user info is returning correctly
@@ -67,16 +66,16 @@ RSpec.describe 'Api::V1::Users', type: :request do
   end
 
   it 'returns an error when the user id in params does not exist (PUT /api/v1/users/:id)' do
-    put '/api/v1/users/4556',
-        params: { first_name: 'abca', last_name: 'abca', email: 'abc4@abc.com', password: '123456', role: role }, 
-        headers: { 'Authorization' => token.to_s }
+    put api_v1_user_url(4556),
+        params: {first_name: 'abca', last_name: 'abca', email: 'abc4@abc.com', password: '123456', role: role }, 
+        headers: bad_auth_header
     expect(response).to have_http_status(:unprocessable_entity)
     parsed = JSON.parse(response.body)
     expect(parsed['error']).to eq("Could not find user with ID '4556'")
   end
 
   it 'deletes an user (DELETE /api/v1/users/:id)' do
-    delete "/api/v1/users/#{user.id}", params: {}, headers: { 'Authorization' => token.to_s }
+    delete api_v1_user_url(user.id), params: {}, headers: good_auth_header
     expect(response).to have_http_status(:success)
   end
 end
