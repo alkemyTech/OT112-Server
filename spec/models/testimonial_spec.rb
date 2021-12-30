4a77d1ef02b1d9a15bb3775378e82af1b9b164ba
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-12-30 00:00:05 -0300

    OT112-102: Testimonial model tests added.

--- spec/models/testimonial_spec.rb
+++ spec/models/testimonial_spec.rb
@@ -12,5 +12,41 @@
 require 'rails_helper'
 
 RSpec.describe Testimonial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{build(:testimonial)}
+
+  context "at creation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "name should be present" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "name should be at least 1 character" do
+      subject.name = "A"
+      expect(subject).to be_valid
+    end
+
+    it "content should be present" do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "content should be at least 1 character" do
+      subject.content = "A"
+      expect(subject).to be_valid
+    end
+
+    it "image may not be present" do
+      subject.image = nil
+      expect(subject).to be_valid
+    end
+
+    it "image should be attached" do
+      expect(subject.image).to be_attached
+    end
+
+  end
 end
