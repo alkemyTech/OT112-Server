8ed7010ddf036202363d6d452308b10afdc55463
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-12-29 23:58:25 -0300

    OT112-102: Testimonials factory updated.

--- spec/factories/testimonials.rb
+++ spec/factories/testimonials.rb
@@ -11,8 +11,27 @@
 #
 FactoryBot.define do
   factory :testimonial do
-    name { "MyString" }
-    content { "MyText" }
-    deleted_at { "2021-11-25 19:49:37" }
+    name { "How Somos Mas changed my life." }
+
+    content { "Lorem ipsum dolor sit amet, consectetur adipiscing elit."\
+              "Nullam tempus, orci non scelerisque lacinia, erat erat"\
+              "consectetur ex, a mollis nisi ante a lacus. Etiam fermentum"\
+              "quis nisi ac efficitur. Lorem ipsum dolor sit amet,"\
+              "consectetur adipiscing elit. Sed odio ante, pharetra aliquam"\
+              "elementum id, consectetur et ex. Quisque vestibulum congue"\
+              "aliquam.\n"\
+              "Vestibulum interdum sem odio, at consectetur leo gravida"\
+              "sed. Nam aliquet neque ac pretium egestas. Nam semper"\
+              "finibus nisl, vel vehicula magna laoreet ac. Fusce luctus"\
+              "nulla vitae mauris pulvinar fermentum sed nec felis. Integer"\
+              "eget neque sed velit ullamcorper tempus nec sed augue." }
+
+    after(:build) do |testimonial|
+      testimonial.image.attach(
+        io: File.open(Rails.root.join('spec', 'images', 'butter.png')),
+        filename: 'butter.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
