b489685d9175b5b3957555e5458d4722dd68e321
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-12-29 23:59:22 -0300

    OT112-102: Testimonials controllers tests added.

--- spec/requests/api/v1/testimonials_spec.rb
+++ spec/requests/api/v1/testimonials_spec.rb
@@ -1,7 +1,94 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Testimonials", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Testimonials', type: :request do
+  let(:role)                { create(:role) }
+  let(:admin_role)          { create(:role, name: 'admin') }
+  let(:user)                { create(:user, role: role) }
+  let(:admin_user)          { create(:user, role: admin_role) }
+  let(:testimonial)         { create(:testimonial) }
+  let(:testimonial_params)  { attributes_for(:testimonial)}
+
+  let(:authorization_header) {
+    {'Authorization' => AuthTokenService.call(user.id).to_s}
+  }
+  let(:admin_authorization_header) {
+    {'Authorization' => AuthTokenService.call(admin_user.id).to_s}
+  }
+
+  context 'with no auth header' do
+    it 'GET must be denied' do
+      get api_v1_testimonials_url
+      expect(response).to have_http_status(401)
+    end
+
+    it 'POST must be denied' do
+      post api_v1_testimonials_url
+      expect(response).to have_http_status(401)
+    end
+
+    it 'DESTROY must be denied' do
+      delete api_v1_testimonial_url(testimonial.id)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'UPDATE must be denied' do
+      put api_v1_testimonial_url(testimonial.id)
+      expect(response).to have_http_status(401)
+    end
   end
+
+  context 'with regular user' do
+    it 'GET  must be Ok' do
+      get api_v1_testimonials_url, headers: authorization_header
+      expect(response).to have_http_status(200)
+    end
+
+    it 'POST must be Denied' do
+      post api_v1_testimonials_url, params: testimonial_params, headers: authorization_header
+      expect(response).to have_http_status(401)
+    end
+
+    it 'DESTROY must be Denied' do
+      delete api_v1_testimonial_url(testimonial.id), headers: authorization_header
+      expect(response).to have_http_status(401)
+    end
+
+    it 'DESTROY on unexistent record must be Denied' do
+      delete api_v1_testimonial_url(-1), headers: authorization_header
+      expect(response).to have_http_status(401)
+    end
+
+    it 'UPDATE must be Denied' do
+      put api_v1_testimonial_url(testimonial.id), headers: authorization_header
+      expect(response).to have_http_status(401)
+    end
+  end
+
+  context 'with admin user' do
+    it 'GET  must be Ok' do
+      get api_v1_testimonials_url, headers: admin_authorization_header
+      expect(response).to have_http_status(200)
+    end
+
+    it 'POST must be Ok' do
+      post api_v1_testimonials_url, params: testimonial_params, headers: admin_authorization_header
+      expect(response).to have_http_status(201)
+    end
+
+    it 'DESTROY must be Ok' do
+      delete api_v1_testimonial_url(testimonial.id), headers: admin_authorization_header
+      expect(response).to have_http_status(200)
+    end
+
+    it 'DESTROY must be not found for unexistent Testimonial' do
+      delete api_v1_testimonial_url(-1), headers: admin_authorization_header
+      expect(response).to have_http_status(404)
+    end
+
+    it 'UPDATE must be Ok' do
+      put api_v1_testimonial_url(testimonial.id), headers: admin_authorization_header
+      expect(response).to have_http_status(201)
+    end
+  end
+
 end
