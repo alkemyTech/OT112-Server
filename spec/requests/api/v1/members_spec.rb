358e9f495643429d61941819f1aaad5594ea40d1
German Vicentin <92609839+gervic08@users.noreply.github.com>
2022-01-03 00:11:21 -0300

    Update spec/requests/api/v1/members_spec.rb
    Co-authored-by: Fernando Ciballero <56528396+FernandoCiballero@users.noreply.github.com>

--- spec/requests/api/v1/members_spec.rb
+++ spec/requests/api/v1/members_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Members", type: :request do
-  let(:role) { FactoryBot.create(:role) }
+  let(:role) { create(:role) }
   let(:role2) { FactoryBot.create(:role, name: "user", description: "user user") }
   
 
